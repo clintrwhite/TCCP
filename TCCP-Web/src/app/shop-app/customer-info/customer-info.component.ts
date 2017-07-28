70053bab4148489f1747838b18203398dd3897ae
Benjamen Alford <benjamen.alford@gmail.com>
2017-07-27 22:07:48 -0500

    stubbed costmer info

--- TCCP-Web/src/app/shop-app/customer-info/customer-info.component.ts
+++ TCCP-Web/src/app/shop-app/customer-info/customer-info.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { User } from '../../shared/models/user';
+import { ShopAppService } from '../shop-app.service';
+import { Component, Input, OnInit } from '@angular/core';
+import 'rxjs'
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-customer-info',
@@ -6,10 +11,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-info.component.css']
 })
 export class CustomerInfoComponent implements OnInit {
+  @Input() user: User;
+  constructor(private appService: ShopAppService, private route: ActivatedRoute, private location: Location) {
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
+  ngOnInit(): void {
+    this.route.paramMap
+      .switchMap((params: ParamMap) => this.appService.getUser(+params.get('id')))
+      .subscribe(u => this.user = u);
+  }
 }
+
+
